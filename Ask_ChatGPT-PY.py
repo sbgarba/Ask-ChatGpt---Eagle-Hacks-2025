651d6fe2779375151190d8f21293d4bb77286b6d
AustinTex09 <akfill9571@eagle.fgcu.edu>
2025-02-22 23:27:39 -0500

    py file draft

--- Ask_ChatGPT-PY.py
+++ Ask_ChatGPT-PY.py
@@ -1,4 +1,4 @@
-import requests                                       # 
+import requests                                       #
 session = requests.Session()
 
 OPENAI_API_KEY = open("API-Ask_ChatGPT", "r").read()
@@ -13,13 +13,17 @@ data = {
         [                                             #   developer - Describe how the model should generally behave
             {                                         #   assistant - Examples for how it should respond
                 "role": "developer",                  #               OR previously generated messages if in a conversation
-                "content": "You are professional doctor with decades of experience."
+                "content": "You are a medical professional with decades of experience who is very smart. "
+                "Read a list of injuries, separated by commas, a single patient is suffering from. "
+                "Rank the list of injuries on a scale of 1-10 based off of severity. "
+                "1 means that the injuries are not urgent and 10 means the injuries require immediate action. Only respond with a number representing the combined severity of the injuries, with a limit of 2 decimal places."
             },
             {
                 "role": "user",
-                "content": "Write a list injuries separated by commas."
+                "content": "Puncture Wound, Dislocation, Laceration."
             }
-        ]
+        ],
+    "temperature" : 0
 }
 
 import json
@@ -27,4 +31,4 @@ json_data = json.dumps(data)
 
 response = session.post(url='https://api.openai.com/v1/chat/completions', data=json_data)
   # 200 = Request Handled OK, 400 = Bad Request, 401 = Unauthorized
-print(response.content.decode('utf8'))
+print(response.content.decode('utf8'))  # get a response from the ai
